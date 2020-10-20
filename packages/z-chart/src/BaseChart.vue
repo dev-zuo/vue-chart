8734670eb9597d00b76040083b7f64d5dd90a22c
guoqzuo <guoqzuo@gmail.com>
2020-10-20 09:49:44 +0800

    delete console.log,update to 0.1.6

--- packages/z-chart/src/BaseChart.vue
+++ packages/z-chart/src/BaseChart.vue
@@ -48,35 +48,26 @@ export default {
 
   methods: {
     init() {
-      console.log(
-        '>>> this.$refs["z-echart"]',
-        this.$refs["z-echart"],
-        this.options
-      );
       let chart = echarts.init(this.$refs["z-echart"]);
-      console.log(">>>chart", chart);
       chart.setOption(this.options || {});
       this.chart = chart;
-      console.log(">>>chart", this.chart);
 
       // 当元素宽高改变时resize执行重绘
       if (this.autoResize) {
         this.__resizeHanlder = debounce(
           () => {
-            console.log(">>> resize", chart);
             chart.resize();
           },
           100,
           { leading: true }
         );
-        console.log(">>>this.$el", this.$el);
+        // console.log(">>>this.$el", this.$el);
         addListener(this.$refs["z-echart"], this.__resizeHanlder);
       }
     },
 
     destroy() {
       // 销毁实例，防止 柱状图 动态切换到 饼图 时，柱状图部分属性依旧留存的问题
-      console.log("destroy");
       this.chart.dispose();
       this.chart = null;
       this.autoResize &&
@@ -85,7 +76,6 @@ export default {
 
     // 销毁后重绘，用于options变更后刷新图表
     refresh() {
-      console.log("refresh");
       this.destroy();
       this.init();
     }
